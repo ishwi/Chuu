32bd37cce0a25c7249b7f4d16146e53310fc20e9
ishwara Coello <ishwara.coello@udc.es>
2019-02-01 16:37:22 +0100

    Initial commit

---
+++ src/main/java/main/LastFMData.java
@@ -0,0 +1,31 @@
+package main;
+
+public class LastFMData {
+
+	private Long showID;
+	private String name;
+
+
+	public LastFMData(String name, Long showID) {
+		this.showID = showID;
+		this.name = name;
+	}
+
+
+	public Long getShowID() {
+		return showID;
+	}
+
+
+	public void setShowID(Long showID) {
+		this.showID = showID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/main/last/UrlCapsule.java
@@ -0,0 +1,21 @@
+package main.last;
+
+public class UrlCapsule {
+
+
+    private final String url;
+    private final int pos;
+
+
+
+    public UrlCapsule(String url, int pos) {
+        this.url = url;
+        this.pos = pos;
+    }
+    public int getPos() {
+        return pos;
+    }
+    public String getUrl() {
+        return url;
+    }
+}
