415779771222ab7fb3ca88d67606e6e10d5c5ef3
ishwara Coello <ishwara.coello@udc.es>
2019-04-06 17:04:01 +0200

    Blurred Background

--- src/main/java/main/ImageRenderer/imageRenderer.java
+++ src/main/java/main/ImageRenderer/imageRenderer.java
@@ -3,14 +3,17 @@ package main.ImageRenderer;
 import DAO.Entities.ResultWrapper;
 import DAO.Entities.Results;
 import DAO.Entities.UserInfo;
+import main.ImageRenderer.Stealing.GaussianFilter;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class imageRenderer {
 	private static final String pathNoImage = "C:\\Users\\Ishwi\\Pictures\\New folder\\818148bf682d429dc215c1705eb27b98.png";
@@ -30,6 +33,17 @@ public class imageRenderer {
 		g.setRenderingHint(
 				RenderingHints.KEY_TEXT_ANTIALIASING,
 				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+		g.setRenderingHint(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
 		a(g);
 
 		//Gets Profile Images
@@ -49,8 +63,8 @@ public class imageRenderer {
 
 
 		//Init Of Variables
-		Font artistFont = new Font("ROBOTO-REGULAR", Font.PLAIN, 19);
-		Font numberFont = new Font("HEEBO-LIGHT", Font.PLAIN, 19);
+		Font artistFont = new Font("ROBOTO-REGULAR", Font.PLAIN, 21);
+		Font numberFont = new Font("HEEBO-LIGHT", Font.PLAIN, 21);
 		Font titleFont = new Font("HEEBO-LIGHT", Font.PLAIN, 23);
 		Font scrobbleFont = new Font("HEEBO-LIGHT", Font.BOLD, 17);
 		int startFont = 26;
@@ -60,8 +74,8 @@ public class imageRenderer {
 		int x = 0;
 		int y = 20;
 		int image1StartPosition, image2StartPosition;
-		image1StartPosition = 30;
-		image2StartPosition = canvas.getWidth() - PROFILE_IMAGE_SIZE - 10;
+		image1StartPosition = 20;
+		image2StartPosition = canvas.getWidth() - PROFILE_IMAGE_SIZE - 20;
 		g.setColor(Color.WHITE);
 		int rectangle_start_x = image1StartPosition + PROFILE_IMAGE_SIZE + 4;
 		int rectangle_start_y = y + PROFILE_IMAGE_SIZE - 20;
@@ -71,9 +85,13 @@ public class imageRenderer {
 		float[] rgb1 = new float[3];
 		Color.ORANGE.getRGBColorComponents(rgb1);
 		Color colorA = new Color(rgb1[0], rgb1[1], rgb1[2], 0.1f);
+		Color colorA1 = new Color(rgb1[0], rgb1[1], rgb1[2], 0.6f);
+
+
 		float[] rgb2 = new float[3];
 		Color.CYAN.getRGBColorComponents(rgb2);
 		Color colorB = new Color(rgb2[0], rgb2[1], rgb2[2], 0.1f);
+		Color colorB1 = new Color(rgb2[0], rgb2[1], rgb2[2], 0.6f);
 
 		g.setFont(usernameFont);
 		int widht1 = g.getFontMetrics().stringWidth(userInfoLiust.get(0).getUsername());
@@ -110,9 +128,9 @@ public class imageRenderer {
 			} else {
 				drawx = image2StartPosition;
 				nameStringPosition = image2StartPosition - width2 - 4;
-				color = colorB;
+				color = colorB.brighter();
 				countStringPosition = image2StartPosition - g.getFontMetrics().stringWidth(String.valueOf(userInfo.getPlaycount())) - 5;
-				rectanglePosition = (int) (image2StartPosition - percentage * userInfo.getPlaycount());
+				rectanglePosition = (int) (image2StartPosition - percentage * rectangle_width) - 4;
 			}
 			g.setColor(color);
 			g.drawImage(image, drawx, y, 100, 100, null);
@@ -152,9 +170,9 @@ public class imageRenderer {
 			int halfDistance = x_MAX - 200;
 			int ac = Math.round((float) countA / (float) (countA + countB) * halfDistance);
 			int bc = Math.round((float) countB / (float) (countA + countB) * halfDistance);
-			g.setColor(Color.orange.brighter());
+			g.setColor(colorA1);
 			g.fillRect(x_MAX / 2 - ac / 2, y + 3, ac / 2, 5);
-			g.setColor(Color.CYAN.brighter());
+			g.setColor(colorB1);
 			g.fillRect(x_MAX / 2, y + 3, bc / 2, 5);
 
 			g.setColor(Color.WHITE);
@@ -180,20 +198,48 @@ public class imageRenderer {
 	private static void a(Graphics2D g) {
 		BufferedImage bim = null;
 		try {
-			bim = ImageIO.read(new File("C:\\Users\\Ishwi\\Desktop\\Wallpaper\\01 - ybtytfq.jpg"));
+			File dir = new File("C:\\Users\\Ishwi\\Desktop\\Wallpaper\\");
+			File[] files = dir.listFiles();
+			Random rand = new Random();
+			assert files != null;
+			File file = files[rand.nextInt(files.length)];
+			bim = ImageIO.read(file);
+			bim = cropImage(bim);
 		} catch (Exception ex) {
 			System.err.println("error in bim " + ex);
 			return;
 		}
-		int wc = bim.getWidth(), hc = bim.getHeight();
 
 		BufferedImage img = new BufferedImage(bim.getWidth(), bim.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = img.createGraphics();
-		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .5f));
 
-		g2.drawImage(bim, 0, 0, null);
+//		//g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .1f));
+//		g2.setComposite(AlphaComposite.DstOut);
+////		int radius = 300;
+////		float fractions[] = {0.0f, 1.0f};
+////		Color colors[] = {new Color(0, 0, 0, 255), new Color(0, 0, 0, 0)};
+////		RadialGradientPaint paint =
+////				new RadialGradientPaint(300, 250, radius, fractions, colors);
+////		g2.setPaint(paint);
+////		g2.drawImage(bim, 0, 0, null);
+//		g2.fillOval(300 - radius, 250 - radius, radius * 2, radius * 2);
+		g2.dispose();
+		g.drawImage(bim, new GaussianFilter(45), 0, 0);
 
-		g.drawImage(bim, 0, 0, null);
+
 	}
 
+
+	private static BufferedImage cropImage(BufferedImage src) {
+		int height = src.getTileHeight();
+		int width = src.getTileWidth();
+		int limity = height - y_MAX;
+		int limitx = width - x_MAX;
+		Random rand = new Random();
+		int x = rand.nextInt(limitx);
+		int y = rand.nextInt(limity);
+		RescaleOp op = new RescaleOp(.8f, 0, null);
+		return op.filter(src.getSubimage(x, y, x_MAX, y_MAX), null);
+
+	}
 }
