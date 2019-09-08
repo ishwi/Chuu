f629ba1f7bed975ed31c7433130c561bd6a3141e
Ishwara Coello <ishwi6@gmail.com>
2019-09-08 23:38:50 +0200

    aliases fixed

--- src/main/java/main/Commands/WhoKnowsSongCommand.java
+++ src/main/java/main/Commands/WhoKnowsSongCommand.java
@@ -55,6 +55,6 @@ public class WhoKnowsSongCommand extends WhoKnowsAlbum {
 
 	@Override
 	public List<String> getAliases() {
-		return Arrays.asList("!wktrack", "!whoknowstrack", "wkt");
+		return Arrays.asList("!wktrack", "!whoknowstrack", "!wkt");
 	}
 }
