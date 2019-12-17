fa4600525586826307ab3a2fe78736831bc5a999
ishwara Coello <ishwara.coello@udc.es>
2019-12-17 23:24:32 +0100

    Ready for Release

---
+++ src/main/java/core/commands/SourceCommand.java
@@ -0,0 +1,36 @@
+package core.commands;
+
+import core.exceptions.InstanceNotFoundException;
+import core.exceptions.LastFmException;
+import dao.DaoImplementation;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SourceCommand extends ConcurrentCommand {
+
+	public SourceCommand(DaoImplementation dao) {
+		super(dao);
+	}
+
+	@Override
+	public String getDescription() {
+		return "Source code of the bot";
+	}
+
+	@Override
+	public List<String> getAliases() {
+		return Collections.singletonList("source");
+	}
+
+	@Override
+	public String getName() {
+		return "Source";
+	}
+
+	@Override
+	void onCommand(MessageReceivedEvent e) throws LastFmException, InstanceNotFoundException {
+		sendMessageQueue(e, "This is the GitHub link of the bot:\nhttps://github.com/ishwi/discordBot");
+	}
+}
---
+++ src/test/java/test/commands/InviteCommandTest.java
@@ -0,0 +1,37 @@
+package test.commands;
+
+import core.Chuu;
+import org.junit.Test;
+import test.commands.utils.CommandTest;
+import test.commands.utils.OneLineUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class InviteCommandTest extends CommandTest {
+	@Override
+	public String giveCommandName() {
+		return "!invite";
+	}
+
+	@Override
+	public void nullParserReturned() {
+
+	}
+
+	@Test
+	public void normalFunctionality() {
+		Properties properties = new Properties();
+		try (InputStream in = Chuu.class.getResourceAsStream("/all.properties")) {
+			properties.load(in);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		Pattern pattern = Pattern.compile("Using the following link you can invite me to your server:\n" +
+				"https://discordapp\\.com/oauth2/authorize\\?scope=bot&client_id=(\\d+){18}&permissions=(?:\\d+)");
+
+		OneLineUtils.testCommands(COMMAND_ALIAS, pattern);
+	}
+}
