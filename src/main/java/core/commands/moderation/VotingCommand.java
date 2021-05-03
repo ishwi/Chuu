e8df58d0347e20774168ffa26f9a771029d81f48
ishwi <ishwi6@gmail.com>
2021-05-03 21:00:33 +0200

    Mod deletiong from vote

--- src/main/java/core/commands/moderation/VotingCommand.java
+++ src/main/java/core/commands/moderation/VotingCommand.java
@@ -21,8 +21,9 @@ import javax.validation.constraints.NotNull;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
 
@@ -32,9 +33,10 @@ public class VotingCommand extends ConcurrentCommand<ArtistParameters> {
     private static final String UP_VOTE = "U+1f44d";
     private static final String DOWN_VOTE = "U+1f44e";
     private static final String REPORT = "U+1f6ab";
+    private static final String CANCEL = "U+1f3f3U+fe0f";
 
 
-    private final BiFunction<JDA, Integer, BiFunction<VotingEntity, EmbedBuilder, EmbedBuilder>> builder = (jda, integer) -> (votingEntity, embedBuilder) ->
+    private final TriFunction<JDA, AtomicInteger, AtomicInteger, BiFunction<VotingEntity, EmbedBuilder, EmbedBuilder>> builder = (jda, size, counter) -> (votingEntity, embedBuilder) ->
             embedBuilder.clearFields()
                     .addField("Added by", CommandUtil.getGlobalUsername(jda, votingEntity.getOwner()), true)
                     .addBlankField(true)
@@ -42,7 +44,7 @@ public class VotingCommand extends ConcurrentCommand<ArtistParameters> {
                     .addField("Points:", String.valueOf(votingEntity.getVotes()), true)
                     .addBlankField(true)
                     .addField("Vote Count:", String.valueOf(votingEntity.getTotalVotes()), true)
-                    .setFooter("Image Count: " + integer + " \nUse \uD83D\uDEAB to report this image")
+                    .setFooter("%d/%d".formatted(counter.get() + 1, size.get()) + "\nUse \uD83D\uDEAB to report this image")
                     .setImage(CommandUtil.noImageUrl(votingEntity.getUrl()))
                     .setColor(CommandUtil.pastelColor());
 
@@ -97,17 +99,12 @@ public class VotingCommand extends ConcurrentCommand<ArtistParameters> {
                 .setTitle(correctedArtist + " Images");
 
         AtomicInteger counter = new AtomicInteger(0);
-        HashMap<String, BiFunction<VotingEntity, MessageReactionAddEvent, Boolean>> actionMap = new HashMap<>();
+        Map<String, BiFunction<VotingEntity, MessageReactionAddEvent, Boolean>> actionMap = new LinkedHashMap<>();
         List<Long> guildList = e.isFromGuild()
                 ? db.getAll(e.getGuild().getIdLong()).stream().filter(u -> !u.getRole().equals(Role.IMAGE_BLOCKED)).map(UsersWrapper::getDiscordID).toList()
                 : List.of(e.getAuthor().getIdLong());
 
-        actionMap.put(REPORT, (a, r) -> {
-            if (guildList.contains(r.getUserIdLong())) {
-                db.report(a.getUrlId(), r.getUserIdLong());
-            }
-            return false;
-        });
+
         actionMap.put(UP_VOTE, (a, r) -> {
             if (guildList.contains(r.getUserIdLong())) {
                 VoteStatus voteStatus = db.castVote(a.getUrlId(), r.getUserIdLong(), true);
@@ -132,45 +129,66 @@ public class VotingCommand extends ConcurrentCommand<ArtistParameters> {
             }
             return false;
         });
+
         if (allArtistImages.size() > 1) {
-            actionMap.put(LEFT_ARROW, (aliasEntity, r) -> {
-                int i = counter.decrementAndGet();
-                if (i == 0) {
-                    r.getReaction().clearReactions().queue();
+            actionMap.put(LEFT_ARROW, (aliasEntity, r) -> leftMove(allArtistImages, counter, r, true));
+            actionMap.put(RIGHT_ARROW, (a, r) -> rightMove(allArtistImages, counter, r, true));
+        }
+        actionMap.put(REPORT, (a, r) -> {
+            if (guildList.contains(r.getUserIdLong())) {
+                db.report(a.getUrlId(), r.getUserIdLong());
+            }
+            return false;
+        });
+        AtomicInteger size = new AtomicInteger(allArtistImages.size());
+
+        if (lastFMData.getRole() == Role.ADMIN) {
+            AtomicInteger deletedCounter = new AtomicInteger();
+            actionMap.put(CANCEL, (a, r) -> {
+                if (r.getUserIdLong() == e.getAuthor().getIdLong()) {
+                    db.removeReportedImage(a.getUrlId(), a.getOwner(), r.getUserIdLong());
+                    counter.decrementAndGet();
+                    allArtistImages.removeIf(ve -> ve.getUrlId() == a.getUrlId());
+                    size.decrementAndGet();
                 }
-                if (i == allArtistImages.size() - 2) {
-                    r.getChannel().addReactionById(r.getMessageIdLong(), RIGHT_ARROW).queue();
-                }
-                return false;
-            });
-            actionMap.put(RIGHT_ARROW, (a, r) -> {
-                int i = counter.incrementAndGet();
-                if (i == allArtistImages.size() - 1) {
-                    r.getReaction().clearReactions().queue();
-                }
-                if (i == 1) {
-                    r.getChannel().addReactionById(r.getMessageIdLong(), LEFT_ARROW).queue();
-                }
+                int i = allArtistImages.size() - 1;
+                if (counter.get() == i && allArtistImages.size() > 0)
+                    rightMove(allArtistImages, counter, r, false);
+                else if (counter.get() < i) {
+                    leftMove(allArtistImages, counter, r, false);
+                }  // Do nothing
                 return false;
             });
         }
-
         new Validator<>(
                 finalEmbed -> {
-                    VotingEntity first = allArtistImages.stream().max(Comparator.comparingLong(VotingEntity::getVotes)).orElse(allArtistImages.get(0));
-                    String description = "Submitted by: " + CommandUtil.getGlobalUsername(e.getJDA(), first.getOwner()) + "\n\n";
-                    if (first != allArtistImages.get(0)) {
-                        description += "The artist image for " + CommandUtil.cleanMarkdownCharacter(first.getArtist()) + " has changed to:";
+                    VotingEntity first = allArtistImages.stream().max(Comparator.comparingLong(VotingEntity::getVotes)).orElse(allArtistImages.isEmpty() ? null : allArtistImages.get(0));
+                    String description;
+                    String title;
+                    if (first == null) {
+                        title = "All images deleted";
+                        description = "This artist no longer has any image";
                     } else {
-                        description += "The top voted image for " + CommandUtil.cleanMarkdownCharacter(first.getArtist()) + " is:";
+                        title = "Voting timed out";
+                        description = "Submitted by: " + CommandUtil.getGlobalUsername(e.getJDA(), first.getOwner()) + "\n\n";
+                        if (first != allArtistImages.get(0)) {
+                            description += "The artist image for " + CommandUtil.cleanMarkdownCharacter(first.getArtist()) + " has changed to:";
+                        } else {
+                            description += "The top voted image for " + CommandUtil.cleanMarkdownCharacter(first.getArtist()) + " is:";
+                        }
                     }
-                    return finalEmbed.setTitle("Voting Timed Out")
-                            .setImage(first.getUrl())
+                    finalEmbed.setTitle(title)
+                            .setImage(first != null ? first.getUrl() : null)
                             .clearFields()
-                            .setDescription(description)
-                            .setFooter(String.format("Has %d %s with %d%s", first.getVotes(), CommandUtil.singlePlural(first.getVotes(), "point", "points"), first.getTotalVotes(),
-                                    CommandUtil.singlePlural(first.getTotalVotes(), " vote", " votes")))
-                            .setColor(ColorService.computeColor(e));
+                            .setDescription(description);
+                    if (first != null) {
+                        finalEmbed.setFooter(String.format("Has %d %s with %d%s", first.getVotes(), CommandUtil.singlePlural(first.getVotes(), "point", "points"), first.getTotalVotes(),
+                                CommandUtil.singlePlural(first.getTotalVotes(), " vote", " votes")))
+                                .setColor(ColorService.computeColor(e));
+                    } else {
+                        finalEmbed.setFooter(null);
+                    }
+                    return finalEmbed;
                 },
                 () -> {
                     if (counter.get() >= allArtistImages.size() - 1) {
@@ -179,11 +197,46 @@ public class VotingCommand extends ConcurrentCommand<ArtistParameters> {
                     if (counter.get() < 0) {
                         counter.set(0);
                     }
+                    if (allArtistImages.isEmpty()) {
+                        return null;
+                    }
                     return allArtistImages.get(counter.get());
                 },
-                builder.apply(e.getJDA(), allArtistImages.size())
+                builder.apply(e.getJDA(), size, counter)
                 , embedBuilder, e.getChannel(), e.getAuthor().getIdLong(), actionMap, true, true);
 
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private Boolean leftMove(List<VotingEntity> allArtistImages, AtomicInteger counter, MessageReactionAddEvent r, boolean isSame) {
+        int i = counter.decrementAndGet();
+        if (i == 0) {
+            if (isSame) {
+                r.getReaction().clearReactions().queue();
+            } else {
+                r.getChannel().removeReactionById(r.getMessageId(), LEFT_ARROW).queue();
+            }
+        }
+        if (i == allArtistImages.size() - 2) {
+            r.getChannel().addReactionById(r.getMessageIdLong(), RIGHT_ARROW).queue();
+        }
+        return false;
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private Boolean rightMove(List<VotingEntity> allArtistImages, AtomicInteger counter, MessageReactionAddEvent r, boolean isSame) {
+        int i = counter.incrementAndGet();
+        if (i == allArtistImages.size() - 1) {
+            if (isSame) {
+                r.getReaction().clearReactions().queue();
+            } else {
+                r.getChannel().removeReactionById(r.getMessageId(), RIGHT_ARROW).queue();
+            }
+        }
+        if (i == 1) {
+            r.getChannel().addReactionById(r.getMessageIdLong(), LEFT_ARROW).queue();
+        }
+        return false;
     }
 }
 
